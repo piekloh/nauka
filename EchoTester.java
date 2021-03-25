f6b1305696b274bdbad8f0e78f45e12b7615c0d8
piekloh <81360745+piekloh@users.noreply.github.com>
2021-03-25 15:21:07 +0100

    Add files via upload

---
+++ EchoTester.java
@@ -0,0 +1,19 @@
+public class EchoTester {
+    public static void main(String[] args) {
+        Echo e1 = new Echo();
+        Echo e2 = e1;
+        int x=0;
+        while(x<4){
+            e1.witaj();
+            e1.ilosc = e1.ilosc + 1;
+            if (x>0){
+            e2.ilosc = e2.ilosc + 1;
+            }
+            if (x>1){
+                e2.ilosc = e2.ilosc + e1.ilosc;
+            }
+            x = x + 1;
+        }
+        System.out.println(e2.ilosc);
+    }
+}
